88273452da51bcca9d3a78102de566c0460c008f
August Skare <post@augustskare.no>
2017-11-18 17:53:52 +0100

    don't run sw.js through babel

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (env) => {
       rules: [
         {
           test: /\.js$/,
-          exclude: /(node_modules)/,
+          exclude: [/(node_modules)/, /sw.js/],
           use: [{
             loader: 'babel-loader',
             options: {
