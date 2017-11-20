564d5f1818cb9fcd6e2473c332294a0d87025f76
August Skare <post@augustskare.no>
2017-11-20 18:42:41 +0100

    removed calculatedReturn util

--- source/javascript/utils/formatter.js
+++ source/javascript/utils/formatter.js
@@ -2,14 +2,6 @@ const percentageFormatter = new Intl.NumberFormat(undefined, { style: 'percent',
 
 const currencyFormatter = (num, nativeCurrency) => nativeCurrency ? new Intl.NumberFormat(undefined, { style: 'currency', currency: nativeCurrency, minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(num) : 0;
 
-const calculatedReturn = (currentValue, purchaseValue) => {
-  let value = 0;
-  if (currentValue && purchaseValue) {
-    value = (currentValue - purchaseValue) / purchaseValue;
-  }
-  return percentageFormatter(value)
-};
-
 const dateFormatter = (date) => date ? date.toLocaleDateString(undefined, { day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric', second: 'numeric' }) : null;
 
-export {percentageFormatter, currencyFormatter, calculatedReturn, dateFormatter}
+export {percentageFormatter, currencyFormatter, dateFormatter}
